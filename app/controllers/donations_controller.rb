a5d84eb348158c5a76c95ed9423486d4e4e32b32
James Byrne <jamesdesbyrne@gmail.com>
2016-11-04 13:21:07 +0000

    Adding the donations controller

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,31 @@
+class DonationsController < ApplicationController
+  include RealexHandlerModule
+  before_action :get_card, only: [:create]
+
+  def index
+  end
+
+  def create
+    amount = params[:amount]
+
+    # Validating the card automatically detects the card type
+    if @card.validate.empty?
+      result = create_purchase(amount, @card)
+
+      render(json: result, status: result[:http_code]) && return
+    end
+    render(json: {error: "credit card invalid"}, status: 402)
+  end
+
+private
+  def get_card
+    @card = create_card({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      number: params[:number],
+      month: params[:month],
+      year: params[:year],
+      verification_value: params[:cvv]
+    })
+  end
+end
